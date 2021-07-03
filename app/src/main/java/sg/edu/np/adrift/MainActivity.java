16ca9fbe5c1680dac8f54a9cd40834cdb52e1da7
hl2703 <s10186258@connect.np.edu.sg>
2021-07-03 21:36:29 +0800

    add bottom nav bar

--- app/src/main/java/sg/edu/np/adrift/MainActivity.java
+++ app/src/main/java/sg/edu/np/adrift/MainActivity.java
@@ -1,8 +1,15 @@
 package sg.edu.np.adrift;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//implement bottom nav bar
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();//hide action bar
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        NavHostFragment navHostFragment =
+                (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView);
+        NavController navController = navHostFragment.getNavController();
+
+        AppBarConfiguration appBarConfiguration =
+                new AppBarConfiguration.Builder(R.id.firstFragment, R.id.secondFragment,R.id.thirdFragment).build();
+
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
     }
 }
