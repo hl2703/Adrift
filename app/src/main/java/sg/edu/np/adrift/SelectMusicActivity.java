b4f258ce52abc23f3224453cf590124cc9ab292d
hl2703 <s10186258@connect.np.edu.sg>
2021-07-05 20:33:03 +0800

    add music page

--- app/src/main/java/sg/edu/np/adrift/SelectMusicActivity.java
+++ app/src/main/java/sg/edu/np/adrift/SelectMusicActivity.java
@@ -1,14 +1,61 @@
 package sg.edu.np.adrift;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
 
 public class SelectMusicActivity extends AppCompatActivity {
 
+    ArrayList<selectObject> objectInfoList = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_music);
+
+        ImageView imageView =findViewById(R.id.backBtn);
+        imageView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                Intent intent = new Intent(SelectMusicActivity.this,MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                startActivity(intent);
+                return false;
+            }
+        });
+
+
+/*
+        myObject obj1= new myObject(R.drawable.pika,"Breathing Exercise");
+        objectInfoList.add(obj1);//first relax feature
+        myObject obj2= new myObject(R.drawable.pika,"Nature Simulation");
+        objectInfoList.add(obj2);//second relax feature
+*/
+
+
+        for (int i=1; i<10;i++){
+
+            selectObject obj = new selectObject(R.drawable.person,String.valueOf(i));
+            objectInfoList.add(obj);
+        }
+
+        RecyclerView recyclerViewCustom = findViewById(R.id.recyclerview2);
+        SelectAdapter sAdapter = new SelectAdapter(objectInfoList);
+        LinearLayoutManager cLayoutManager = new LinearLayoutManager(this);
+        recyclerViewCustom.setLayoutManager(cLayoutManager);
+        recyclerViewCustom.setItemAnimator(new DefaultItemAnimator());
+        recyclerViewCustom.setAdapter(sAdapter);
     }
+
 }
+
+
+
