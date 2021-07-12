c0a5b6486d975fb54cc60b25f838446967b8cb15
hl2703 <s10186258@connect.np.edu.sg>
2021-07-12 18:46:02 +0800

    edit audio page layout

---
+++ app/src/main/java/sg/edu/np/adrift/AudioActivity.java
@@ -0,0 +1,67 @@
+package sg.edu.np.adrift;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+public class AudioActivity extends AppCompatActivity {
+    MediaPlayer mediaPlayer;
+    ImageButton imgB;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_audio);
+
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.sleepaudio);
+        mediaPlayer.setLooping(true);
+
+        mediaPlayer.start();
+
+        //pause play func
+        //Button pButton=findViewById(R.id.pButton);
+        imgB = findViewById(R.id.imageButton);
+        imgB.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+
+                if (mediaPlayer.isPlaying()) {
+                    mediaPlayer.pause();
+
+
+                    imgB.setImageResource(R.drawable.play);
+
+                } else {
+                    mediaPlayer.start();
+
+                    imgB.setImageResource(R.drawable.pause);
+                }
+            }
+        });
+//back btn
+        ImageView imageView = findViewById(R.id.backBtn);
+        imageView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                Intent intent = new Intent(AudioActivity.this, MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                startActivity(intent);
+                return false;
+            }
+        });
+        // }
+    }
+
+    protected void onPause() {
+        super.onPause();
+        mediaPlayer.stop();
+        mediaPlayer.release();
+
+    }
+
+}
